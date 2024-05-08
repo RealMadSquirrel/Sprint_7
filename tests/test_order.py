a3d1d5b5e8e15b6f367286d0de94016a948961c3
RealMadSquirrel <olga.y.belova@yandex.ru>
2024-05-08 10:13:36 +0300

    Финальный проект спринта №7
        -- Добавлен новый allure-отчет
        -- Изменение в названии тестов в файле test_order.py(была ошибка)

--- tests/test_order.py
+++ tests/test_order.py
@@ -15,13 +15,13 @@ class TestOrder:
         pytest.param(["BLACK", "GREY"]),
         pytest.param(" ")
     ])
-    def test_success_create_courier(self, color):
+    def test_success_create_order(self, color):
         payload = ChangeTestDataHelper.modify_payload_body("color", color)
         created_order_request = OrderApi.create_order(payload)
         order_id = created_order_request.json()["track"]
         assert created_order_request.status_code == 201 and order_id is not None and order_id > 0
 
     @allure.title("Проверка получения списка товаров")
-    def test_success_create_courier(self):
+    def test_success_list_order(self):
         list_order_request = OrderApi.get_list_order()
         assert list_order_request.status_code == 200 and list_order_request.json()["orders"] != []
